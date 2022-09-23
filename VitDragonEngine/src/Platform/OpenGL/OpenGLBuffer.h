566ae6c45d8e8ab488808cdb34cb7cbac6fa2dd3
Bruno Vitte <bruno.vitte@itam.mx>
2022-09-23 11:36:37 -0500

    Add a vertex buffer layout

--- VitDragonEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ VitDragonEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -12,8 +12,12 @@ namespace VitDragonEngine{
 		virtual void Bind() const;
 		virtual void UnBind() const;
 
+		virtual const BufferLayout &GetLayout() const override{ return m_Layout; }
+		virtual void SetLayout( const BufferLayout &layout ) override{ m_Layout = layout; }
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer{
@@ -21,8 +25,8 @@ namespace VitDragonEngine{
 		OpenGLIndexBuffer( uint32_t *vertices, uint32_t size );
 		virtual ~OpenGLIndexBuffer();
 		
-		virtual void Bind() const;
-		virtual void UnBind() const;
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
 
 		virtual uint32_t GetCount() const{ return m_Count; };
 
--- VitDragonEngine/src/VitDragonEngine/Application.cpp
+++ VitDragonEngine/src/VitDragonEngine/Application.cpp
@@ -13,6 +13,25 @@ namespace VitDragonEngine{
 
 	Application *Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType( ShaderDataType type ){
+		switch( type ){
+			case ShaderDataType::Float:		return GL_FLOAT;
+			case ShaderDataType::Float2:	return GL_FLOAT;
+			case ShaderDataType::Float3:	return GL_FLOAT;
+			case ShaderDataType::Float4:	return GL_FLOAT;
+			case ShaderDataType::Mat3:		return GL_FLOAT;
+			case ShaderDataType::Mat4:		return GL_FLOAT;
+			case ShaderDataType::Int:		return GL_INT;
+			case ShaderDataType::Int2:		return GL_INT;
+			case ShaderDataType::Int3:		return GL_INT;
+			case ShaderDataType::Int4:		return GL_INT;
+			case ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		VDE_CORE_ASSERT( false, "Unknown ShaderDataType!" );
+		return 0;
+	}
+
 	VitDragonEngine::Application::Application(){
 		VDE_CORE_ASSERT( !s_Instance, "Application already exists" );
 		s_Instance = this;
@@ -27,16 +46,44 @@ namespace VitDragonEngine{
 		glGenVertexArrays( 1, &m_VertexArray );
 		glBindVertexArray( m_VertexArray );
 
-		float vertices[ 3 * 3 ] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[ 3 * 7 ] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create( vertices, sizeof( vertices ) ));
 
-		glEnableVertexAttribArray( 0 );
-		glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( float ), nullptr );
+		{
+		
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position"},
+				{ ShaderDataType::Float4, "a_Color"}
+			};
+
+			m_VertexBuffer->SetLayout( layout );
+		}
+
+		uint32_t index = 0;
+		const auto &layout = m_VertexBuffer->GetLayout();
+		for( const auto &element : layout ){
+
+			glEnableVertexAttribArray( index );
+			glVertexAttribPointer( 
+				index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(), 
+				(const void *)element.Offset
+			);
+			index++;
+
+		}
+
+
+		//m_VertexBuffer->SetLayout(layout);
+
 
 		unsigned int indices[ 3 ] = { 0, 1, 2 };
 		m_IndexBuffer.reset( IndexBuffer::Create( indices, sizeof( indices ) / sizeof( uint32_t ) ) );
@@ -45,12 +92,15 @@ namespace VitDragonEngine{
 			#version 330 core	
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main(){
 				v_Position = a_Position;
-				gl_Position = vec4(a_Position, 1.0);					
+				v_Color = a_Color;
+				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -60,9 +110,11 @@ namespace VitDragonEngine{
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main(){
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);					
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
