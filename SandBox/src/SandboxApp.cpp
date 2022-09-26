8b94bd5825a3f2c7087b561c63ef1b92588d24b6
Bruno Vitte <bruno_vitte@hotmail.com>
2022-09-25 21:52:43 -0500

    add transforms

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public VitDragonEngine::Layer{
 public:
 	ExampleLayer()
@@ -34,10 +36,10 @@ public:
 
 		m_SquareVA.reset( VitDragonEngine::VertexArray::Create() );
 		float squareVertices[ 3 * 4 ] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 		std::shared_ptr<VitDragonEngine::VertexBuffer> squareVB;
 		squareVB.reset( VitDragonEngine::VertexBuffer::Create( squareVertices, sizeof( squareVertices ) ) );
@@ -58,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -65,7 +68,7 @@ public:
 			void main(){
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -91,12 +94,13 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main(){
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -139,9 +143,6 @@ public:
 		else if( VitDragonEngine::Input::IsKeyPressed( VDE_KEY_D ) )
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 
-
-
-
 		VitDragonEngine::RenderCommand::SetClearColor( { 0.1f, 0.1f, 0.1f, 1 } );
 		VitDragonEngine::RenderCommand::Clear();
 
@@ -150,7 +151,15 @@ public:
 
 		VitDragonEngine::Renderer::BeginScene( m_Camera );
 
-		VitDragonEngine::Renderer::Submit( m_BlueShader, m_SquareVA );
+		static glm::mat4 scale = glm::scale( glm::mat4( 1.0f ), glm::vec3( 0.1f ) );
+
+		for( int x = 0; x < 20; x++ ){
+			for( int y = 0; y < 20; y++ ){
+				glm::vec3 pos( x * 0.11f, y * 0.11f, 0.0f );
+				glm::mat4 transform = glm::translate( glm::mat4( 1.0f ), pos) * scale;
+				VitDragonEngine::Renderer::Submit( m_BlueShader, m_SquareVA, transform );
+			}
+		}
 
 		VitDragonEngine::Renderer::Submit( m_Shader, m_VertexArray );
 
@@ -174,11 +183,10 @@ private:
 
 	VitDragonEngine::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
+	float m_CameraMoveSpeed = 5.0f;
 	
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
-	
-	float m_CameraMoveSpeed = 5.0f;
 };
 
 class SandBox : public VitDragonEngine::Application{
